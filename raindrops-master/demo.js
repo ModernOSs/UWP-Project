b90149ec48406519785e6e227ae9ddbcaa28fd87
18826073587@163.com <18826073587@163.com>
2016-04-16 21:57:51 +0800

    将水波界面移植到UWP
    在UWP上实现了水波，但是不支持缩放和水位变化

--- raindrops-master/demo.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    $('#demo').height($(window).height());
-    jQuery('#demo').raindrops({
-        canvasHeight: $(window).height(),
-        waveLength: 100,
-        rippleSpeed: 0.05
-    });
-}
