f30014e0c1407d5f4d4e614117cf310a47f2338e
刘忍 <18826073587@163.com>
2016-05-07 10:53:30 +0800

    修改js
    修改js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,47 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+var users = {'admin': '12345678'};
 
 app.post('/', function (req, res) {
-  if (req.headers.username == "shuqianqian" && req.headers.password == "123456")
+  console.log("login info: ", req.headers.username, req.headers.password);
+  if (users[req.headers.username] == undefined) {
+    res.send('no user');
+    console.log("no user");
+  } else {
+    if (users[req.headers.username] == req.headers.password) {
+      res.send('success');
+      console.log("success");
+    } else {
+      res.send('wrong password');
+      console.log("wrong password");
+    }
+  }
+});
+
+app.post('/register', function(req, res) {
+  console.log("register info: ", req.headers.username, req.headers.password);
+  if (users[req.headers.username] != undefined) {
+    res.send('already exists');
+    console.log("already exists");
+  } else {
+    users[req.headers.username] = req.headers.password;
     res.send('success');
-  else
-    res.send('fail');
+    console.log(users);
+  }
+});
+
+app.get('*', function (req, res) {
+  res.send('闈炴硶璁块棶!');
+});
+
+app.post('*', function (req, res) {
+  res.send('闈炴硶璁块棶!');
 });
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at ', port);
 });
